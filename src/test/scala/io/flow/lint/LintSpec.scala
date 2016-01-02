dcf52ab62a39fcaee1af778f8d9c8088414ab094
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-02 17:58:31 -0500

    Add helpers class

--- src/test/scala/io/flow/lint/LintSpec.scala
+++ src/test/scala/io/flow/lint/LintSpec.scala
@@ -2,35 +2,7 @@ package io.flow.lint
 
 import org.scalatest.{FunSpec, Matchers}
 
-class LintSpec extends FunSpec with Matchers {
-
-  val healthcheckResourceTemplate = """
-    {
-      "type": "io.flow.common.v0.models.healthcheck",
-      "plural": "healthchecks",
-      "operations": [
-        {
-          "method": "%s",
-          "path": "%s",
-          "parameters": [],
-          "responses": [
-            {
-              "code": {
-                "integer": {
-                  "value": %s
-                }
-              },
-              "type": "%s"
-            }
-          ]
-        }
-      ]
-    }
-  """
-
-  val healthcheck = ServiceBuilder().addResource(
-    healthcheckResourceTemplate.format("GET", "/_internal_/healthcheck", 200, "io.flow.common.v0.models.healthcheck")
-  )
+class LintSpec extends FunSpec with Matchers with Helpers {
 
   it("temporary debugging") {
     Lint.fromFile("/tmp/organization.json").validate() match {
