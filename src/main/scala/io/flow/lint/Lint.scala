9f8062e3b2a5a0c0fd53f870702e93dee43defdf
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-02 13:31:41 -0500

    Parse service from files

--- src/main/scala/io/flow/lint/Lint.scala
+++ src/main/scala/io/flow/lint/Lint.scala
@@ -1,6 +1,26 @@
 package io.flow.lint
 
 import com.bryzek.apidoc.spec.v0.models.Service
+import com.bryzek.apidoc.spec.v0.models.json._
+import play.api.libs.json._
+
 
 case class Lint(service: Service) {
+
+  service.models.foreach { m =>
+    println(s"m: ${m.name}")
+  }
+
+}
+
+object Lint extends App {
+
+  fromFile("/tmp/organization.json")
+
+  def fromFile(path: String): Lint = {
+    val contents = scala.io.Source.fromFile(path).getLines.mkString("\n")
+    val service = Json.parse(contents).as[Service]
+    Lint(service)
+  }
+
 }
