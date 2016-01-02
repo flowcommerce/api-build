24d7d2af65602faa77f79473669b50c04013e2fa
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-02 17:51:23 -0500

    Add healthcheck

--- src/test/scala/io/flow/lint/LintSpec.scala
+++ src/test/scala/io/flow/lint/LintSpec.scala
@@ -4,6 +4,30 @@ import org.scalatest.{FunSpec, Matchers}
 
 class LintSpec extends FunSpec with Matchers {
 
+  val healthcheck = ServiceBuilder().addResource("""
+    {
+      "type": "io.flow.common.v0.models.healthcheck",
+      "plural": "healthchecks",
+      "operations": [
+        {
+          "method": "GET",
+          "path": "/_internal_/healthcheck",
+          "parameters": [],
+          "responses": [
+            {
+              "code": {
+                "integer": {
+                  "value": 200
+                }
+              },
+              "type": "io.flow.common.v0.models.healthcheck"
+            }
+          ]
+        }
+      ]
+    }
+  """)
+
   it("my test") {
     Lint.fromFile("/tmp/organization.json").validate() match {
       case Nil => println("valid")
@@ -20,6 +44,10 @@ class LintSpec extends FunSpec with Matchers {
     Lint(ServiceBuilder().service).validate should be(
       Seq("Missing resource: healthchecks")
     )
+  }
+
+  it("healthcheck service is valid") {
+    Lint(healthcheck.service).validate should be(Nil)
   }
 
 }
