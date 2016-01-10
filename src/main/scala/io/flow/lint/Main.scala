432a5cc5cd5ff3ce81db674193d85698817ecb22
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-10 09:23:40 -0500

    Better error msg for invalid args

--- src/main/scala/io/flow/lint/Main.scala
+++ src/main/scala/io/flow/lint/Main.scala
@@ -17,6 +17,8 @@ object Main extends App {
     }
   }
 
+  private[this] case class Config(organization: String, application: String, version: String)
+
   ApidocConfig.load() match {
     case Left(error) => println(s"** Error loading apidoc config: $error")
     case Right(config) => {
@@ -25,34 +27,37 @@ object Main extends App {
         import scala.concurrent.ExecutionContext.Implicits.global
 
         args.foreach { name =>
-          val (organization, application, version) = name.split("/").map(_.trim).toList match {
-            case org :: app :: Nil => (org, app, "latest")
-            case org :: app :: version :: Nil => (org, app, version)
+          val configOption = name.split("/").map(_.trim).toList match {
+            case org :: app :: Nil => Some(Config(org, app, "latest"))
+            case org :: app :: version :: Nil => Some(Config(org, app, version))
             case _ => {
-              sys.error(s"Invalid name[$name] - expected organization/application (e.g. flow/user)")
+              println(s"** ERROR: Invalid name[$name] - expected organization/application (e.g. flow/user)")
+              None
             }
           }
 
-          println("")
-          println(s"$name")
-          print(s"  Downloading...")
-          dl.service(organization, application, version) match {
-            case Left(error) => {
-              addError(organization, application, error)
-              println("\n  ** ERROR: " + error)
-            }
-            case Right(service) => {
-              print("  Done\n  Starting Linter... ")
-              linter.validate(service) match {
+          configOption.map { config =>
+            println("")
+            println(s"$name")
+            print(s"  Downloading...")
+            dl.service(config.organization, config.application, config.version) match {
+              case Left(error) => {
+                addError(config.organization, config.application, error)
+                println("\n  ** ERROR: " + error)
+              }
+              case Right(service) => {
+                print("  Done\n  Starting Linter... ")
+                linter.validate(service) match {
                 case Nil => println("\n  Valid!")
-                case errors => {
-                  errors.size match {
+                  case errors => {
+                    errors.size match {
                     case 1 => println(" 1 error:")
-                    case n => println(s" $n errors:")
-                  }
-                  errors.sorted.foreach { error =>
-                    addError(organization, application, error)
-                    println(s"    - $error")
+                      case n => println(s" $n errors:")
+                    }
+                    errors.sorted.foreach { error =>
+                      addError(config.organization, config.application, error)
+                      println(s"    - $error")
+                    }
                   }
                 }
               }
