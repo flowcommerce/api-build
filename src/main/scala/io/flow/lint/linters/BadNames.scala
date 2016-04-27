8aba7a11cf5d1a57c874eb9e883b39a0cced5be1
Michael Bryzek <mbryzek@alum.mit.edu>
2016-04-27 09:14:50 -0400

    Add linter for bad names (e.g. country of origin => origin) (#27)

---
+++ src/main/scala/io/flow/lint/linters/BadNames.scala
@@ -0,0 +1,49 @@
+package io.flow.lint.linters
+
+import io.flow.lint.Linter
+import com.bryzek.apidoc.spec.v0.models.{Field, Model, Operation, Parameter, Resource, Service}
+import scala.util.{Failure, Success, Try}
+
+/**
+  *  We have decided to call the same things consistently. This linter
+  *  validates common field and parameter names
+  */
+case object BadNames extends Linter with Helpers {
+
+  val Data = Map(
+    "country_of_origin" -> "origin",
+    "ip_address" -> "ip",
+    "postal_code" -> "postal"
+  )
+
+  override def validate(service: Service): Seq[String] = {
+    service.models.flatMap(validateModel(service, _)) ++ service.resources.flatMap(validateResource(service, _))
+  }
+
+  def validateModel(service: Service, model: Model): Seq[String] = {
+    model.fields.flatMap { validateField(model, _) }
+  }
+
+  def validateField(model: Model, field: Field): Seq[String] = {
+    Data.get(field.name) match {
+      case None => Nil
+      case Some(replacement) => Seq(error(model, field, s"Name must be '$replacement'"))
+    }
+  }
+
+  def validateResource(service: Service, resource: Resource): Seq[String] = {
+    resource.operations.flatMap(validateOperation(service, resource, _))
+  }
+
+  def validateOperation(service: Service, resource: Resource, operation: Operation): Seq[String] = {
+    operation.parameters.flatMap(validateParameter(service, resource, operation, _))
+  }
+
+  def validateParameter(service: Service, resource: Resource, operation: Operation, param: Parameter): Seq[String] = {
+    Data.get(param.name) match {
+      case None => Nil
+      case Some(replacement) => Seq(error(resource, operation, param, s"Name must be '$replacement'"))
+    }
+  }
+
+}
