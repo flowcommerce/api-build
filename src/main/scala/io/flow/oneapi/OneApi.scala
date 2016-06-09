dd5613bfd2206748ae844c7f4b1531e81e398c24
Michael Bryzek <mbryzek@alum.mit.edu>
2016-06-08 20:40:07 -0400

    Remove trailing period in 401 description

--- src/main/scala/io/flow/oneapi/OneApi.scala
+++ src/main/scala/io/flow/oneapi/OneApi.scala
@@ -24,7 +24,7 @@ case class OneApi(services: Seq[Service]) {
     "200" -> "Successful response",
     "201" -> "Operation succeeded and the resource was created",
     "204" -> "Operation succeeded. No content is returned",
-    "401" -> "Authorization failed.",
+    "401" -> "Authorization failed",
     "404" -> "Resource was not found",
     "422" -> "One or more errors were found with the data sent in the request. The body of the response contains specific details on what data failed validation."
   )
