b453db10f443570f1b618375d1449dccafc7635d
Christian Muñoz <christian.munoz@flow.io>
2016-06-21 19:17:07 -0400

    Add rule to validate sort attribute exists when operation has sort parameter

---
+++ src/test/scala/io/flow/lint/SortAttributeSpec.scala
@@ -0,0 +1,57 @@
+package io.flow.lint
+
+import com.bryzek.apidoc.spec.v0.models._
+import org.scalatest.{FunSpec, Matchers}
+import play.api.libs.json.Json
+
+class SortAttributeSpec extends FunSpec with Matchers {
+
+  val linter = linters.SortAttribute
+
+  val sortParameter = Parameter(
+    name = "sort",
+    `type` = "string",
+    location = ParameterLocation.Query,
+    required = false,
+    default = None
+  )
+
+  val sortAttribute = Attribute(
+    name = "sort",
+    Json.obj()
+  )
+
+  val otherAttribute = Attribute(
+    name = "other",
+    Json.obj()
+  )
+
+  def buildService(parameter: Parameter, attribute: Attribute): Service = {
+    Services.Base.copy(
+      resources = Seq(
+        Services.buildSimpleResource(
+          `type` = "organization",
+          plural = "organizations",
+          method = Method.Get,
+          path = "/organization",
+          responseCode = 200,
+          responseType = "[organization]",
+          parameters = Seq(parameter),
+          attributes = Seq(attribute)
+        )
+      )
+    )
+  }
+
+  it("should be empty when sort attribute exists") {
+    linter.validate(
+      buildService(sortParameter, sortAttribute)
+    ) should be (Nil)
+  }
+
+  it("should have errors when sort attribute is missing") {
+    linter.validate(
+      buildService(sortParameter, otherAttribute)
+    ) should be(Seq("Resource organizations GET /organization: Missing attribute named sort"))
+  }
+}
