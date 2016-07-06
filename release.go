9f905b4eb04ab8bb3899b493f9410e33ed8e0c17
Michael Bryzek <mbryzek@alum.mit.edu>
2016-07-06 09:49:48 -0400

    Rename api-lint to api-build

--- release.go
+++ release.go
@@ -5,19 +5,19 @@ import (
 )
 
 func main() {
-	executor := executor.Create("api-lint")
+	executor := executor.Create("api-build")
 
 	executor = executor.Add("cd ../aws-s3-public\ngit checkout master")
 	executor = executor.Add("cd ../aws-s3-public\ngit pull --rebase")
 	executor = executor.Add("cd ../aws-s3-public\ngit fetch --tags origin")
 
-	executor = executor.Add("dev tag --label micro")
+	executor = executor.Add("dev tag")
 	executor = executor.Add("sbt clean one-jar")
-	executor = executor.Add("cp ./target/scala-2.11/api-lint*-one-jar.jar ../aws-s3-public/util/api-lint/")
-	executor = executor.Add("cp ./target/scala-2.11/api-lint*-one-jar.jar ../aws-s3-public/util/api-lint/api-lint.jar")
+	executor = executor.Add("cp ./target/scala-2.11/api-build*-one-jar.jar ../aws-s3-public/util/api-build/")
+	executor = executor.Add("cp ./target/scala-2.11/api-build*-one-jar.jar ../aws-s3-public/util/api-build/api-build.jar")
 
-	executor = executor.Add("cd ../aws-s3-public\ngit add util/api-lint/*")
-	executor = executor.Add("cd ../aws-s3-public\ngit commit -m 'Add new version of api-lint' util/api-lint")
+	executor = executor.Add("cd ../aws-s3-public\ngit add util/api-build/*")
+	executor = executor.Add("cd ../aws-s3-public\ngit commit -m 'Add new version of api-build' util/api-build")
 	executor = executor.Add("cd ../aws-s3-public\ngit push origin master")
 	executor = executor.Add("cd ../aws-s3-public\naws s3 sync util s3://io.flow.aws-s3-public/util --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
 
