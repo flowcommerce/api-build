a463b95ffefeb9d651ef44d8be7bbe57c0e76a12
Michael Bryzek <mbryzek@alum.mit.edu>
2016-08-01 22:47:14 -0400

    Add linter for lower case paths (#61)

---
+++ src/test/scala/io/flow/lint/LowerCasePathsSpec.scala
@@ -0,0 +1,46 @@
+package io.flow.lint
+
+import com.bryzek.apidoc.spec.v0.models._
+import org.scalatest.{FunSpec, Matchers}
+
+class LowerCasePathsSpec extends FunSpec with Matchers {
+
+  val linter = linters.LowerCasePaths
+
+  def buildService(
+    path: String
+  ): Service = {
+    Services.Base.copy(
+      resources = Seq(
+        Resource(
+          `type` = "organization",
+          plural = "organizations",
+          operations = Seq(
+            Operation(
+              method = Method.Get,
+              path = path,
+              parameters = Nil,
+              responses = Seq(Services.buildResponse(`type` = "unit"))
+            )
+          )
+        )
+      )
+    )
+  }
+
+  it("lower case paths are good") {
+    linter.validate(
+      buildService("/organizations/tiers/:tier_id")
+    ) should be(Nil)
+  }
+
+  it("Flags upper case paths") {
+    linter.validate(
+      buildService("/organizations/tiers/:tierId")
+    ) should be(
+      Seq(
+        "Resource organizations GET /organizations/tiers/:tierId: Path must be all lower case"
+      )
+    )
+  }
+}
