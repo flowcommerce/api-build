16e098aa0c574d52c55eb68a849a781b39a2bbb0
Michael Bryzek <mbryzek@alum.mit.edu>
2016-10-24 08:40:42 -0400

    Consider union types for error verification (#102)

--- src/test/scala/io/flow/lint/ErrorModelsSpec.scala
+++ src/test/scala/io/flow/lint/ErrorModelsSpec.scala
@@ -23,7 +23,30 @@ class ErrorModelsSpec extends FunSpec with Matchers {
     )
   }
 
-  it("fields must start with code, messages") {
+  def buildServiceWithUnion(
+    fields: Seq[Field],
+    discriminator: Option[String] = Some("code")
+  ): Service = {
+    Services.Base.copy(
+      unions = Seq(
+        Services.buildUnion(
+          "user_error",
+          discriminator = discriminator,
+          types = Seq(
+            Services.buildUnionType("user_auth_error")
+          )
+        )
+      ),
+      models = Seq(
+        Services.buildModel(
+          name = "user_auth_error",
+          fields = fields
+        )
+      )
+    )
+  }
+
+  it("standalone model fields must start with code, messages") {
     linter.validate(buildService(Seq(code, messages))) should be(Nil)
 
     linter.validate(buildService(Nil)) should be(Seq(
@@ -43,12 +66,32 @@ class ErrorModelsSpec extends FunSpec with Matchers {
     ))
   }
 
-  it("validated type of 'code' field") {
+  it("standalone model validated type of 'code' field") {
     linter.validate(buildService(Seq(code.copy(`type` = "integer"), messages))) should be(Seq(
       "Model test_error Field[code]: type must be 'string'"
     ))
     linter.validate(buildService(Seq(code, messages.copy(`type` = "integer")))) should be(Seq(
       "Model test_error Field[messages]: type must be '[string]'"
+    ))
+  }
+
+  it("unions") {
+    linter.validate(buildServiceWithUnion(Seq(messages))) should be(Nil)
+
+    linter.validate(buildServiceWithUnion(Seq(messages, code))) should be(Seq(
+      "Model user_auth_error: field named 'code' must come from union type discriminator"
+    ))
+
+    linter.validate(buildServiceWithUnion(Seq(messages.copy(`type` = "integer")))) should be(Seq(
+      "Model user_auth_error Field[messages]: type must be '[string]'"
+    ))
+
+    linter.validate(buildServiceWithUnion(Seq(messages.copy(name = "foo")))) should be(Seq(
+      "Model user_auth_error: requires a field named 'messages'"
+    ))
+
+    linter.validate(buildServiceWithUnion(Seq(code.copy(name = "foo"), messages))) should be(Seq(
+      "Model user_auth_error: first field must be 'messages'"
     ))
   }
 
