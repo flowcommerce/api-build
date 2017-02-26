cb96ee41a32ceabc612c965f3c227aecbc8c40a7
Michael Bryzek <mbryzek@alum.mit.edu>
2017-02-26 11:17:39 -0500

    Include model name in event errors

--- src/main/scala/io/flow/lint/linters/EventModels.scala
+++ src/main/scala/io/flow/lint/linters/EventModels.scala
@@ -27,34 +27,34 @@ case object EventModels extends Linter with Helpers {
 
       case "event_id" :: "timestamp" :: "organization" :: rest => {
         fieldNames.contains("number") match {
-          case true => Seq("number field must come after organization in event models")
+          case true => Seq(error(model, "number field must come after organization in event models"))
           case false => Nil
         }
       }
 
       case "event_id" :: "timestamp" :: rest => {
-        validateOrgAndNumber(rest)
+        validateOrgAndNumber(model, rest)
       }
 
       case _ => {
         val timestampErrors = fieldNames.contains("timestamp") match {
-          case true => Seq("timestamp field must come after event_id in event models")
-          case false => Seq("timestamp field is required in event models")
+          case true => error(model, "timestamp field must come after event_id in event models")
+          case false => error(model, "timestamp field is required in event models")
         }
 
-        timestampErrors ++ validateOrgAndNumber(fieldNames)
+        Seq(timestampErrors) ++ validateOrgAndNumber(model, fieldNames)
       }
     }
   }
 
-  private[this] def validateOrgAndNumber(fieldNames: Seq[String]): Seq[String] = {
+  private[this] def validateOrgAndNumber(model: Model, fieldNames: Seq[String]): Seq[String] = {
     val orgErrors = fieldNames.contains("organization") match {
-      case true => Seq("organization field must come after timestamp in event models")
+      case true => Seq(error(model, "organization field must come after timestamp in event models"))
       case false => Nil
     }
 
     val numberErrors = fieldNames.contains("number") match {
-      case true => Seq("organization field is required if event model has a field named number")
+      case true => Seq(error(model, "organization field is required if event model has a field named number"))
       case false => Nil
     }
 
--- src/test/scala/io/flow/lint/EventModelsSpec.scala
+++ src/test/scala/io/flow/lint/EventModelsSpec.scala
@@ -21,30 +21,30 @@ class EventModelsSpec extends FunSpec with Matchers {
 
   it("fields") {
     linter.validate(buildService(Seq("event_id"))) should be(Seq(
-      "timestamp field is required in event models"
+      "Model org_upserted: timestamp field is required in event models"
     ))
 
     linter.validate(buildService(Seq("event_id", "foo", "timestamp"))) should be(Seq(
-      "timestamp field must come after event_id in event models"
+      "Model org_upserted: timestamp field must come after event_id in event models"
     ))
 
     linter.validate(buildService(Seq("event_id", "timestamp"))) should be(Nil)
 
     linter.validate(buildService(Seq("event_id", "timestamp", "number"))) should be(Seq(
-      "organization field is required if event model has a field named number"
+      "Model org_upserted: organization field is required if event model has a field named number"
     ))
 
     linter.validate(buildService(Seq("event_id", "timestamp", "organization"))) should be(Nil)
 
     linter.validate(buildService(Seq("event_id", "timestamp", "foo", "organization"))) should be(Seq(
-      "organization field must come after timestamp in event models"
+      "Model org_upserted: organization field must come after timestamp in event models"
     ))
 
     linter.validate(buildService(Seq("event_id", "timestamp", "organization", "number"))) should be(Nil)
     linter.validate(buildService(Seq("event_id", "timestamp", "organization", "number", "foo"))) should be(Nil)
 
     linter.validate(buildService(Seq("event_id", "timestamp", "organization", "foo", "number"))) should be(Seq(
-      "number field must come after organization in event models"
+      "Model org_upserted: number field must come after organization in event models"
     ))
   }
 
