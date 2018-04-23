4473ee5000059de8f75d80c222212f0b90a9ee46
Michael Bryzek <mike@flow.io>
2018-04-23 13:46:33 -0400

    Lint trailing slace on path

--- src/test/scala/io/flow/lint/LowerCasePathsSpec.scala
+++ src/test/scala/io/flow/lint/LowerCasePathsSpec.scala
@@ -7,36 +7,15 @@ class LowerCasePathsSpec extends FunSpec with Matchers {
 
   private[this] val linter = linters.LowerCasePaths
 
-  def buildService(
-    path: String
-  ): Service = {
-    Services.Base.copy(
-      resources = Seq(
-        Resource(
-          `type` = "organization",
-          plural = "organizations",
-          operations = Seq(
-            Operation(
-              method = Method.Get,
-              path = path,
-              parameters = Nil,
-              responses = Seq(Services.buildResponse(`type` = "unit"))
-            )
-          )
-        )
-      )
-    )
-  }
-
   it("lower case paths are good") {
     linter.validate(
-      buildService("/organizations/tiers/:tier_id")
+      Services.buildServiceByPath("/organizations/tiers/:tier_id")
     ) should be(Nil)
   }
 
   it("Flags upper case paths") {
     linter.validate(
-      buildService("/organizations/tiers/:tierId")
+      Services.buildServiceByPath("/organizations/tiers/:tierId")
     ) should be(
       Seq(
         "Resource organizations GET /organizations/tiers/:tierId: Path must be all lower case"
