65b67c3ea1f3a5cb57c12bda78b21ee8362f3ff8
Michael Bryzek <mbryzek@alum.mit.edu>
2018-06-29 23:07:46 -0400

    Host shopify session on session service (#190)

--- src/main/scala/io/flow/proxy/Controller.scala
+++ src/main/scala/io/flow/proxy/Controller.scala
@@ -14,7 +14,8 @@ case class Controller() extends io.flow.build.Controller {
 
   private[this] val HostingMap = Map(
     "optin"->"content",
-    "consumer-invoice" -> "order-messenger"
+    "consumer-invoice" -> "order-messenger",
+    "shopify-session" -> "session"
   )
 
   /**
