d2cd6f804940551f0b2ee9e82c2c7b5aaf82f0c9
Gregor Heine <gregor@flow.io>
2018-09-27 21:44:16 +0100

    Remove duplicate underscore

--- src/main/scala/io/flow/stream/Controller.scala
+++ src/main/scala/io/flow/stream/Controller.scala
@@ -72,7 +72,7 @@ case class Controller() extends io.flow.build.Controller {
               } else {
                 val serviceMajorVersion = VersionParser.parse(service.version).major.getOrElse(0)
                 val internal = if (service.name.contains("-internal-") && !union.name.contains("_internal_")) "internal_" else ""
-                val shortName = s"${union.name}_${internal}_v${serviceMajorVersion}"
+                val shortName = s"${union.name}_${internal}v${serviceMajorVersion}"
                 val allModels = multiService.services.flatMap(_.service.models)
                 val allUnions = multiService.services.flatMap(_.service.unions)
                 val allEnums = multiService.services.flatMap(_.service.enums)
