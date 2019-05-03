2dfcda4d25c93c535d035ddaa1d9198a34da2d47
Michael Bryzek <mike@flow.io>
2019-05-03 09:19:11 -0400

    wip

--- src/main/scala/io/flow/lint/util/Expansions.scala
+++ src/main/scala/io/flow/lint/util/Expansions.scala
@@ -13,15 +13,16 @@ object Expansions extends Helpers {
     *   Seq("id", "expandable_organization") => Seq("organization")
     */
   def fromFieldTypes(fields: Seq[String]): Seq[String] = {
-    fields.flatMap(toName(_))
+    fields.flatMap(toName)
   }
 
   private[this] def toName(field: String): Option[String] = {
     val idx = field.lastIndexOf(".")
     val name = if (idx < 0) { baseType(field) } else { baseType(field.substring(idx + 1)) }
-    name.startsWith("expandable_") match {
-      case true => Some(name.replace("expandable_", ""))
-      case false => None
+    if (name.startsWith("expandable_")) {
+      Some(name.replace("expandable_", ""))
+    } else {
+      None
     }
   }
 
