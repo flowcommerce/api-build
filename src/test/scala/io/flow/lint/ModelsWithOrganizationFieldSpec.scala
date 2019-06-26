3692d15833c9fcf232d0ed8afcdc500d966062f9
Michael Bryzek <mike@flow.io>
2019-06-26 16:58:49 +0100

    Do not lint position of organization fields if optional

--- src/test/scala/io/flow/lint/ModelsWithOrganizationFieldSpec.scala
+++ src/test/scala/io/flow/lint/ModelsWithOrganizationFieldSpec.scala
@@ -19,6 +19,22 @@ class ModelsWithOrganizationFieldSpec extends FunSpec with Matchers {
     linter.validate(buildService(Seq("id", "email"))) should be(Nil)
   }
 
+  it("no-op if organization field is optional") {
+    linter.validate(
+      Services.Base.copy(
+        models = Seq(
+          Services.buildModel(
+            name = "user",
+            fields = Seq(
+              Services.buildField(name = "foobar"),
+              Services.buildField(name = "organization", required = false)
+            )
+          )
+        )
+      )
+    ) should be(Nil)
+  }
+
   it("w/ organization but no id fields") {
     linter.validate(buildService(Seq("other", "organization"))) should be(Seq(
       "Model user: Field[organization] must be in position[0] and not[1]"
