e98878f8f7f6d3a75cf8bf01322f9c0ece0c6cc8
Matthew Mark Miller <m3@dasmb.com>
2020-08-07 12:22:45 -0400

    Adding interface support to OneApi API merge

--- src/main/scala/io/flow/oneapi/OneApi.scala
+++ src/main/scala/io/flow/oneapi/OneApi.scala
@@ -137,6 +137,10 @@ case class OneApi(
         s.models.map(localizeModel(parser, _))
       }.sortBy { _.name.toLowerCase },
 
+      interfaces = services.flatMap { s =>
+        s.interfaces.map(localizeInterface(parser, _))
+      }.sortBy { _.name.toLowerCase },
+
       unions = services.flatMap { s =>
         s.unions.map(localizeUnion(parser, _))
       }.sortBy { _.name.toLowerCase },
@@ -275,6 +279,12 @@ case class OneApi(
   def localizeModel(parser: TextDatatypeParser, model: Model): Model = {
     model.copy(
       fields = model.fields.map(localizeField(parser, _))
+    )
+  }
+
+  def localizeInterface(parser: TextDatatypeParser, interface: Interface): Interface = {
+    interface.copy(
+      fields = interface.fields.map(localizeField(parser, _))
     )
   }
 
