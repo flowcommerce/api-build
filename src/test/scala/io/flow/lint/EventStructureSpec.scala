cf1979b418956135ff060cf87ade38b596e77fac
Michael Bryzek <mike@flow.io>
2023-03-06 13:22:00 -0500

    wip

--- src/test/scala/io/flow/lint/EventStructureSpec.scala
+++ src/test/scala/io/flow/lint/EventStructureSpec.scala
@@ -1,6 +1,6 @@
 package io.flow.lint
 
-import io.apibuilder.spec.v0.models.{Field, Model, UnionType}
+import io.apibuilder.spec.v0.models.{Field, Model, Union, UnionType}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -13,6 +13,11 @@ class EventStructureSpec extends AnyFunSpec with Matchers {
       Services.buildField("id")
     )
   )
+  private[this] val userEventUnion: Union = Services.buildUnion("user_event", types = Seq(
+    Services.buildUnionType("user_upserted"),
+    Services.buildUnionType("user_deleted"),
+  ))
+
   private[this] def buildEventModel(name: String, fields: Seq[Field]): Model = {
     Services.buildModel(
       name,
@@ -25,40 +30,56 @@ class EventStructureSpec extends AnyFunSpec with Matchers {
 
   it("upserted events have matching deleted events") {
     def setup(deleteModel: Option[Model], deleteUnionType: Option[UnionType]) = {
-      Services.Base.copy(
-        models = Seq(userModel) ++ Seq(
-          buildEventModel("user_upserted", Seq(
-            Services.buildField("user", `type` = "user")
-          ))
-        ) ++ deleteModel.toSeq,
-        unions = Seq(
-          Services.buildUnion("user_event", types = Seq(
-            Services.buildUnionType("user_upserted")
-          ) ++ deleteUnionType.toSeq)
+      linter.validate(
+        Services.Base.copy(
+          models = Seq(userModel) ++ Seq(
+            buildEventModel("user_upserted", Seq(
+              Services.buildField("user", `type` = "user")
+            ))
+          ) ++ deleteModel.toSeq,
+          unions = Seq(
+            Services.buildUnion("user_event", types = Seq(
+              Services.buildUnionType("user_upserted")
+            ) ++ deleteUnionType.toSeq)
+          )
         )
       )
     }
 
-    linter.validate(
-      setup(deleteModel = None, deleteUnionType = None)
-    ) shouldBe Seq("Missing delete event for 'user_upserted'")
+    setup(deleteModel = None, deleteUnionType = None) shouldBe Seq(
+      "Missing delete event for 'user_upserted'"
+    )
 
-    linter.validate(
-      setup(deleteModel = Some(
-        buildEventModel("user_deleted", Seq(
-          Services.buildField("id", `type` = "string")
-        ))
-      ), deleteUnionType = Some(
-        Services.buildUnionType("user_deleted")
+    setup(deleteModel = Some(
+      buildEventModel("user_deleted", Seq(
+        Services.buildField("id")
       ))
-    ) shouldBe Nil
+    ), deleteUnionType = Some(
+      Services.buildUnionType("user_deleted")
+    )) shouldBe Nil
   }
 
   it("upserted events have matching deleted events spanning version numbers") {
     //"user_upserted_v4" and "user_deleted_v1"
   }
 
-    it("deleted_events must have an 'id' string field") {
+  it("deleted_events must have an 'id' string field") {
+    def setup(field: Field) = {
+      linter.validate(
+        Services.Base.copy(
+          unions = Seq(userEventUnion),
+          models = Seq(userModel) ++ Seq(
+            buildEventModel("user_upserted", Seq(
+              Services.buildField("user", `type` = "user")
+            )),
+            buildEventModel("user_deleted", Seq(field))
+          )
+        )
+      )
+    }
+
+    setup(Services.buildField("id", `type` = "string")) shouldBe Nil
+    setup(Services.buildField("user", `type` = "user")) shouldBe Seq("TODO")
   }
 
   it("events have no additional fields") {
